63ac090a19c727937da423603b6fa038eac1f312
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-06-14 18:30:43 +0530

    added some comments

--- db.js
+++ db.js
@@ -1,47 +1,56 @@
 const Sequelize = require('sequelize')
 
+//declaring a database
 const db = new Sequelize({
     dialect: 'sqlite',
     storage: __dirname + '/test.db',
 })
 
+// declaring and defining the structure of tasks model
+
 const Tasks = db.define('Tasks', {
-    title: {
-        type: Sequelize.STRING(100),
+    title: {                                    //for title of task
+        type: Sequelize.STRING(100),        
         allowNull: false,
     },
-    description: {
+    description: {                              // for the description of tasks
         type: Sequelize.STRING(200),
         allowNull: true
     },
-    due: {
+    due: {                                      // stores the due date 
         type: Sequelize.DATEONLY
     },
-    priority: {
+    priority: {                                 // stores the priority as high, medium or low
         type: Sequelize.STRING(30),
         allowNull: false
     },
-    status: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
+    status: {                                   // stores status as boolean  
+        type: Sequelize.BOOLEAN,                // incomplete as false 
+        defaultValue: false                     // completed as true
     }
 })
 
+//declaring and defining the structure of Notes model
+
 const Notes = db.define('Notes', {
-    task_id: {
-        type: Sequelize.INTEGER,
+    task_id: {                                  // stores the task_id 
+        type: Sequelize.INTEGER,                // in case two tasks have same note
         primaryKey: true,
         autoIncrement: true
     },
-    content: {
+    content: {                                  // stores the value of the description of the note
         type: Sequelize.STRING(100),
         allowNull: false,
     }
 })
 
+// these two statements help link the two models 
+// every task can have any number of notes
 Tasks.hasMany(Notes)
+// every note belongs to one task
 Notes.belongsTo(Tasks)
 
+// exporting the database and models
 module.exports = {
     db, Tasks,Notes
 }
--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const {db,Tasks,Notes} = require('./db')
 
 const app = express()
 
+// this is helpful if the app is hosted
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8000;
 }
+
+// checked if the database is successfully synced or not
 
 db.sync().then(() => {
     app.listen(port)
@@ -16,29 +19,34 @@ db.sync().then(() => {
         console.error(err)
     })
 
-
+// middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// declaring the static folder
 app.use('/', express.static(__dirname + '/front-end'))
 
 //gets all the task
-
 app.get('/tasks',async(req,res) =>{
-    const tasks = await Tasks.findAll({include: Notes})
+    // we extract all the notes including their Notes 
+    const tasks = await Tasks.findAll({include: Notes})         
 
+    //error handling
     if(tasks===null)
         return res.status(400).send("error");
 
     res.send(tasks)
 })
+
 // adds a task to the database
 
 app.post('/addTask',async(req,res) =>{
+    //checked if title is not a string or is empty
     if (typeof req.body.title !== 'string' || req.body.title =='') {
         return res.status(400).send({ error: 'Task name must be provided' })
     }
 
+    // created a new task with the input the user has entered
     const newTask = await Tasks.create({
         title: req.body.title,
         description: req.body.description,
@@ -53,10 +61,12 @@ app.post('/addTask',async(req,res) =>{
 // adds a note to the respective task
 
 app.post('/:id/addNote', async (req, res) => {
+    //checked if title is not a string or is empty
     if (typeof req.body.content !== 'string' || req.body.content == '') {
         return res.status(400).send({ error: 'Note must be provided' })
     }
 
+    // created a new note to the respective Task
     const newTask = await Notes.create({
         TaskId: Number(req.params.id),
         content: req.body.content
@@ -68,6 +78,8 @@ app.post('/:id/addNote', async (req, res) => {
 // updates the task with the respective id
 
 app.put('/update',async(req,res) =>{
+
+    // updates the content of given task
     Tasks.update({   
             title: req.body.title,
             description: req.body.description,
@@ -84,6 +96,8 @@ app.put('/update',async(req,res) =>{
     
     return res.status(200).send('succesfully updated');
 })
+
+//for getting a respective task with given id
 
 app.get('/:id/task', async (req, res) => {
     const tasks = await Tasks.findByPk(Number(req.params.id))
